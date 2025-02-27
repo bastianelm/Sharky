9e964d31cc538ede0560a147ff9af548f70e33f4
bastianelm <elmbastian@proton.me>
2025-02-27 11:01:07 +0100

    Objects (PufferFish and Character) Scaling

--- js/models/Character.js
+++ js/models/Character.js
@@ -1,4 +1,7 @@
 class Character extends MoveableObject{
+    height=250;
+    width=250;
+    y = 200;
     constructor(){
         super().loadImage('../img/1.Sharkie/3.Swim/1.png');
     }
--- js/models/PufferFish.js
+++ js/models/PufferFish.js
@@ -1,4 +1,8 @@
 class PufferFish extends MoveableObject{
+    y = 350;
+    height = 60;
+    width = 86;
+    
     constructor(){
         super().loadImage('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
         this.x = 720 - this.width;
