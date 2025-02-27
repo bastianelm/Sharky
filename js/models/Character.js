fca7e6a794106831ab9d49e83c19899d6f38ed6d
bastianelm <elmbastian@proton.me>
2025-02-27 17:12:33 +0100

    added keyboard functionality

--- js/models/Character.js
+++ js/models/Character.js
@@ -11,6 +11,7 @@ class Character extends MoveableObject {
         '../../img/1.Sharkie/3.Swim/5.png',
         '../../img/1.Sharkie/3.Swim/6.png',
     ];
+    world;
 
     constructor() {
         super().loadImage('../../img/1.Sharkie/3.Swim/1.png');
