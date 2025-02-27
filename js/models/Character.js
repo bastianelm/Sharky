2bf49b896ee6a9f27300f65905778390597b9886
bastianelm <elmbastian@proton.me>
2025-02-27 17:38:14 +0100

    only animate when movement is done

--- js/models/Character.js
+++ js/models/Character.js
@@ -22,10 +22,12 @@ class Character extends MoveableObject {
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.IMAGE_SWIMMING.length;
-            let path = this.IMAGE_SWIMMING[i]; // Use the correct index
-            this.img = this.imageCache[path]; // Update the current image
-            this.currentImage++;
+            if(this.world.keyboard.RIGHT){
+                let i = this.currentImage % this.IMAGE_SWIMMING.length;
+                let path = this.IMAGE_SWIMMING[i]; // Use the correct index
+                this.img = this.imageCache[path]; // Update the current image
+                this.currentImage++;
+            }
         }, 150);
     }
 }
