b0eb08a6eab59fcc87ea1a04cc7a707c65396e1a
bastianelm <elmbastian@proton.me>
2025-04-04 13:58:19 +0200

    added some images to Endboss.js

--- js/models/Endboss.js
+++ js/models/Endboss.js
@@ -2,9 +2,7 @@ class Endboss extends MoveableObject{
 
     width = 300;
     height = 300;
-
-
-    IMAGE_SWIMMING = [
+    IMAGE_INTRODUCE = [
         '../../img/2.Enemy/3 Final Enemy/1.Introduce/1.png',
         '../../img/2.Enemy/3 Final Enemy/1.Introduce/2.png',
         '../../img/2.Enemy/3 Final Enemy/1.Introduce/3.png',
@@ -15,6 +13,21 @@ class Endboss extends MoveableObject{
         '../../img/2.Enemy/3 Final Enemy/1.Introduce/8.png',
         '../../img/2.Enemy/3 Final Enemy/1.Introduce/9.png',
         '../../img/2.Enemy/3 Final Enemy/1.Introduce/10.png',
+    ]
+    IMAGE_SWIMMING = [
+        '../../img/2.Enemy/3 Final Enemy/2.floating/1.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/2.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/3.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/4.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/5.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/6.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/7.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/8.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/9.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/10.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/11.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/12.png',
+        '../../img/2.Enemy/3 Final Enemy/2.floating/13.png',
     ];
     constructor(){
         super().loadImage(this.IMAGE_SWIMMING[0]);
