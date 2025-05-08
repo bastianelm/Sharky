51b8ada71d9b09dd8cd3ec6ea319f29e894a4cbf
bastianelm <elmbastian@proton.me>
2025-05-08 17:21:14 +0200

    updated StatusBar

--- js/models/StatusBar.js
+++ js/models/StatusBar.js
@@ -1,5 +1,18 @@
-class StatusBar extends DrawaableObject{
+alert("asdfsss");
+class StatusBar extends DrawableObject{
+    IMAGES = [
+        'img/4. Marcadores/green/Life/0_ copia 3.png',
+        'img/4. Marcadores/green/Life/20_ copia 4.png',
+        'img/4. Marcadores/green/Life/40_ copia 3.png',
+        'img/4. Marcadores/green/Life/60_ copia 3.png',
+        'img/4. Marcadores/green/Life/80 copia 3.png',
+        'img/4. Marcadores/green/Life/100_ copia 2.png',
+    ];
     constructor(){
-
+        this.loadImages(IMAGES);
+    }
+    percentage = 100;
+    setPercentage(p){
+        this.percentage = p;
     }
 }
