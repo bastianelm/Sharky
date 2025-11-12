515ae9996e8dcb8e87a87b0b517483da5f7c6f84
bastianelm <elmbastian@proton.me>
2025-11-12 12:25:50 +0100

    changed Endboss.js

--- js/models/Endboss.js
+++ js/models/Endboss.js
@@ -14,7 +14,7 @@ class Endboss extends MoveableObject {
         'img/2.Enemy/3 Final Enemy/1.Introduce/7.png',
         'img/2.Enemy/3 Final Enemy/1.Introduce/8.png',
         'img/2.Enemy/3 Final Enemy/1.Introduce/9.png',
-        'img/2.Enemy/3 Final Enemy/1.Introduce/10.png',
+        'img/2.Enemy/3 Final Enemy/1.Introduce/10.png'
     ];
 
     IMAGE_SWIMMING = [
@@ -30,7 +30,7 @@ class Endboss extends MoveableObject {
         'img/2.Enemy/3 Final Enemy/2.floating/10.png',
         'img/2.Enemy/3 Final Enemy/2.floating/11.png',
         'img/2.Enemy/3 Final Enemy/2.floating/12.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/13.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/13.png'
     ];
 
     IMAGE_ATTACK = [
@@ -39,7 +39,7 @@ class Endboss extends MoveableObject {
         'img/2.Enemy/3 Final Enemy/Attack/3.png',
         'img/2.Enemy/3 Final Enemy/Attack/4.png',
         'img/2.Enemy/3 Final Enemy/Attack/5.png',
-        'img/2.Enemy/3 Final Enemy/Attack/6.png',
+        'img/2.Enemy/3 Final Enemy/Attack/6.png'
     ];
 
     IMAGE_DEATH = [
@@ -48,12 +48,9 @@ class Endboss extends MoveableObject {
     ];
 
     /**
-     * Determines if the Endboss and the character occupy the same position
-     * within the given tolerances.
-     *
-     * @param {number} toleranceX - Allowed horizontal tolerance in pixels.
-     * @param {number} toleranceY - Allowed vertical tolerance in pixels.
-     * @returns {boolean} True if both X and Y positions are within tolerance, otherwise false.
+     * @param {number} toleranceX - Horizontal tolerance in pixels
+     * @param {number} toleranceY - Vertical tolerance in pixels
+     * @returns {boolean} True if Endboss is within distance of character
      */
     isLowDistance(toleranceX, toleranceY) {
         const lowX = this.x === world.character.x - toleranceX;
@@ -62,18 +59,14 @@ class Endboss extends MoveableObject {
     }
 
     /**
-     * Moves the Endboss towards the player's character based on its current position
-     * and a tolerance value that depends on the Endboss's remaining lives.
-     *
+     * Moves the Endboss toward the player based on current position and lives.
      * @returns {void}
      */
     followCharacter() {
         const tolerance = this.lives >= 2000 ? 250 : 50;
-        if (this.y < world.character.y - tolerance) {
-            this.moveDown();
-        } else {
-            this.moveUp();
-        }
+        if (this.y < world.character.y - tolerance) this.moveDown();
+        else this.moveUp();
+
         if (this.x < world.character.x - tolerance) {
             this.otherDirection = true;
             this.moveRight();
@@ -84,8 +77,7 @@ class Endboss extends MoveableObject {
     }
 
     /**
-     * Plays the attack animation for the Endboss.
-     *
+     * Plays the attack animation.
      * @returns {void}
      */
     attack() {
@@ -93,13 +85,11 @@ class Endboss extends MoveableObject {
     }
 
     /**
-     * Starts the movement loop of the Endboss, following the character
-     * and triggering attack animations.
-     *
+     * Handles movement and attack behavior over time.
      * @returns {void}
      */
     move() {
-        this.moveInterval = setStoppableInterval(() => {
+        this.moveInterval = setInterval(() => {
             if (this.isDead) {
                 clearInterval(this.moveInterval);
                 return;
@@ -110,11 +100,11 @@ class Endboss extends MoveableObject {
     }
 
     /**
-     * Creates a new Endboss instance, loads all required images,
-     * and starts its movement behavior.
+     * Initializes the Endboss with all assets loaded and behavior started.
      */
     constructor() {
         super();
+        this.attack = this.attack.bind(this);
         this.loadImage(this.IMAGE_INTRODUCE[0]);
         this.loadImages(this.IMAGE_INTRODUCE);
         this.loadImages(this.IMAGE_SWIMMING);
